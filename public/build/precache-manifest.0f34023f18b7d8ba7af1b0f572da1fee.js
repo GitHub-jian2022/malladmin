33b01be39c0c68f5cf06786365393f8fd0e79b4b
zengweipei <zengwp_youwu@163.com>
2020-09-13 16:39:53 +0800

    build

---
+++ public/build/precache-manifest.0f34023f18b7d8ba7af1b0f572da1fee.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d47218110837334d1fd1c1ba73801b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a565e60a62c2e22d74d",
+    "url": "/static/css/0.3cdcdd57.chunk.css"
+  },
+  {
+    "revision": "22692a648aea6efeece7",
+    "url": "/static/css/1.921d9331.chunk.css"
+  },
+  {
+    "revision": "b8a98bc929fbf5c7bf42",
+    "url": "/static/css/10.7629380f.chunk.css"
+  },
+  {
+    "revision": "b4f2268dd764577830d7",
+    "url": "/static/css/11.1720dc40.chunk.css"
+  },
+  {
+    "revision": "14435a124e800159995b",
+    "url": "/static/css/12.4b80ea58.chunk.css"
+  },
+  {
+    "revision": "81ec3dbccc63ff985ce4",
+    "url": "/static/css/13.b55e7bf0.chunk.css"
+  },
+  {
+    "revision": "7994cb52a07f84f60b9c",
+    "url": "/static/css/14.fd9ef421.chunk.css"
+  },
+  {
+    "revision": "e28d2d0d94f40e97d381",
+    "url": "/static/css/15.fd9ef421.chunk.css"
+  },
+  {
+    "revision": "be813858d367d6af7222",
+    "url": "/static/css/16.e73ab31f.chunk.css"
+  },
+  {
+    "revision": "55e7ac8052e8b1873a46",
+    "url": "/static/css/17.9715cfa2.chunk.css"
+  },
+  {
+    "revision": "93819361326f3eb28f05",
+    "url": "/static/css/18.08acb408.chunk.css"
+  },
+  {
+    "revision": "4d5bc6fd8c07e44b757e",
+    "url": "/static/css/19.76cfceaa.chunk.css"
+  },
+  {
+    "revision": "e37ae69b3a56ab9919fa",
+    "url": "/static/css/2.d7e16c76.chunk.css"
+  },
+  {
+    "revision": "f83bdcbb441581248582",
+    "url": "/static/css/20.8e5ef89f.chunk.css"
+  },
+  {
+    "revision": "0db9e5842d552b304d56",
+    "url": "/static/css/21.c5b38f32.chunk.css"
+  },
+  {
+    "revision": "167cd497b164afbfb441",
+    "url": "/static/css/22.744543aa.chunk.css"
+  },
+  {
+    "revision": "0523df1487c8ea790eb7",
+    "url": "/static/css/23.19ebec3b.chunk.css"
+  },
+  {
+    "revision": "4874f1c0c55bcbd7fedb",
+    "url": "/static/css/24.f678b32d.chunk.css"
+  },
+  {
+    "revision": "bcb6f5b659fc646d280d",
+    "url": "/static/css/25.e9699257.chunk.css"
+  },
+  {
+    "revision": "faffccdb38964c41ba38",
+    "url": "/static/css/4.fd9ef421.chunk.css"
+  },
+  {
+    "revision": "f0ac91eaedd0386e40b5",
+    "url": "/static/css/5.3b4f9bce.chunk.css"
+  },
+  {
+    "revision": "cf5d1ebc335419ab5eee",
+    "url": "/static/css/8.59d20406.chunk.css"
+  },
+  {
+    "revision": "0219e7367a7293fc9e50",
+    "url": "/static/css/main.830c96ea.chunk.css"
+  },
+  {
+    "revision": "8a565e60a62c2e22d74d",
+    "url": "/static/js/0.b478074f.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/0.b478074f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22692a648aea6efeece7",
+    "url": "/static/js/1.29a368ed.chunk.js"
+  },
+  {
+    "revision": "b8a98bc929fbf5c7bf42",
+    "url": "/static/js/10.90883f53.chunk.js"
+  },
+  {
+    "revision": "b4f2268dd764577830d7",
+    "url": "/static/js/11.4514dbb8.chunk.js"
+  },
+  {
+    "revision": "14435a124e800159995b",
+    "url": "/static/js/12.a55325bc.chunk.js"
+  },
+  {
+    "revision": "81ec3dbccc63ff985ce4",
+    "url": "/static/js/13.497cc23a.chunk.js"
+  },
+  {
+    "revision": "7994cb52a07f84f60b9c",
+    "url": "/static/js/14.750abe3a.chunk.js"
+  },
+  {
+    "revision": "e28d2d0d94f40e97d381",
+    "url": "/static/js/15.e9df6ef8.chunk.js"
+  },
+  {
+    "revision": "be813858d367d6af7222",
+    "url": "/static/js/16.a9a00727.chunk.js"
+  },
+  {
+    "revision": "55e7ac8052e8b1873a46",
+    "url": "/static/js/17.ac481cba.chunk.js"
+  },
+  {
+    "revision": "93819361326f3eb28f05",
+    "url": "/static/js/18.d4bc9c8b.chunk.js"
+  },
+  {
+    "revision": "4d5bc6fd8c07e44b757e",
+    "url": "/static/js/19.666d1f4f.chunk.js"
+  },
+  {
+    "revision": "e37ae69b3a56ab9919fa",
+    "url": "/static/js/2.b4e9700d.chunk.js"
+  },
+  {
+    "revision": "97636669c8193ace9a3b4f1548fc4d0b",
+    "url": "/static/js/2.b4e9700d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83bdcbb441581248582",
+    "url": "/static/js/20.89b34c5a.chunk.js"
+  },
+  {
+    "revision": "0db9e5842d552b304d56",
+    "url": "/static/js/21.ce2ec91e.chunk.js"
+  },
+  {
+    "revision": "167cd497b164afbfb441",
+    "url": "/static/js/22.1d01d8ea.chunk.js"
+  },
+  {
+    "revision": "0523df1487c8ea790eb7",
+    "url": "/static/js/23.ff8f8569.chunk.js"
+  },
+  {
+    "revision": "4874f1c0c55bcbd7fedb",
+    "url": "/static/js/24.d0db9800.chunk.js"
+  },
+  {
+    "revision": "bcb6f5b659fc646d280d",
+    "url": "/static/js/25.b928d13f.chunk.js"
+  },
+  {
+    "revision": "b9cae9c0678d53a4f9eb",
+    "url": "/static/js/3.aed0d970.chunk.js"
+  },
+  {
+    "revision": "faffccdb38964c41ba38",
+    "url": "/static/js/4.7f4eaed1.chunk.js"
+  },
+  {
+    "revision": "f0ac91eaedd0386e40b5",
+    "url": "/static/js/5.e5ca9101.chunk.js"
+  },
+  {
+    "revision": "0372b2c0baa0a5692aba80048fff8e6c",
+    "url": "/static/js/5.e5ca9101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5d1ebc335419ab5eee",
+    "url": "/static/js/8.6f435a75.chunk.js"
+  },
+  {
+    "revision": "2b9a1550dc39bc051a48",
+    "url": "/static/js/9.f555df4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/9.f555df4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0219e7367a7293fc9e50",
+    "url": "/static/js/main.24b6f041.chunk.js"
+  },
+  {
+    "revision": "a741a5905b97e895b779",
+    "url": "/static/js/runtime-main.24d13c79.js"
+  },
+  {
+    "revision": "73d3109ffabfc5d80adb6b46738603cc",
+    "url": "/static/media/1.73d3109f.jpg"
+  },
+  {
+    "revision": "f193ef75fc044affb537548224912f29",
+    "url": "/static/media/address-b.f193ef75.svg"
+  },
+  {
+    "revision": "63e13fb81bf5d63d9fa7a1445d4445e2",
+    "url": "/static/media/defalutAvatar.63e13fb8.jpg"
+  },
+  {
+    "revision": "b56560d0cf9c872013843a22b9b33e4c",
+    "url": "/static/media/purse.b56560d0.png"
+  }
+]);
--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -7,13 +7,13 @@ const auth = require('../../../middleware')
 
 router.post('/login', async (ctx) => {
     const { phone, password } = ctx.request.body
-    
+
     let res = fs.readFileSync(path.join(__dirname, '../../../static/user/user.json'), 'utf8')
     let { data } = JSON.parse(res)
     let userId = ''
     let flag = false
-    flag = data.some((item,index) => {
-        if(item.phone === phone && decrypt(item.password) === decrypt(password)) {
+    flag = data.some((item, index) => {
+        if (item.phone === phone && decrypt(item.password) === decrypt(password)) {
             userId = item.id
             return true
         }
@@ -34,7 +34,7 @@ router.post('/login', async (ctx) => {
     }
 })
 
-router.post('/getUserInfo',auth, async (ctx) => {
+router.post('/getUserInfo', auth, async (ctx) => {
     let { token } = ctx.request.body
     token = verify(token).decoded
     console.log('token: ', token);
@@ -43,7 +43,7 @@ router.post('/getUserInfo',auth, async (ctx) => {
     let { data } = JSON.parse(res)
     let user = {}
     data.some(item => {
-        if(item.id === userId) {
+        if (item.id === userId) {
             user = item
         }
     })
@@ -51,6 +51,39 @@ router.post('/getUserInfo',auth, async (ctx) => {
         msg: 'success',
         code: 200,
         data: user
+    }
+})
+
+router.post('/register', async (ctx) => {
+    const { phone, password } = ctx.request.body
+
+    let res = fs.readFileSync(path.join(__dirname, '../../../static/user/user.json'), 'utf8')
+    res = JSON.parse(res)
+    let flag = false
+    flag = res.data.some((item) => item.phone === phone)
+    if (flag) {
+        //用户已存在
+        ctx.body = {
+            code: 401,
+            msg: '账号已存在',
+        }
+    } else {
+        // 写入json保存
+        let user = {
+            id: res.data.length + 1,
+            phone,
+            password,
+            username: 'user_'+phone
+        }
+        res.data.push(user)
+        let str = JSON.stringify(res, null, "\t");
+        fs.writeFile(path.join(__dirname, '../../../static/user/user.json'), str, err => {
+            err && console.log(err);
+        })
+        ctx.body = {
+            code: 200,
+            msg: '注册成功'
+        }
     }
 })
 
